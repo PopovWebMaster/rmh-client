d86e8d63ae3155bb58b0a39d7dcce8854163b499
Alexandr Popov <popovalexandr84@mail.ru>
2025-03-28 17:37:18 +0300

    28 03 2025

--- src/js/components/LoginPage/LoginPage.js
+++ src/js/components/LoginPage/LoginPage.js
@@ -7,18 +7,129 @@ import './LoginPage.scss';
 
 import { PageContainer } from './../PageContainer/PageContainer.js';
 
+import { send_request_to_server } from './../../helpers/send_request_to_server.js';
+
 import { ROUTE } from './../../config/routes.js';
 
 const LoginPageComponent = ( props ) => {
 
     let {
+
     } = props;
-    
+
+    let [ email, setEmail ] = useState( '' );
+    let [ password, setPassword ] = useState( '' );
+
+    const changeEmail = ( e ) => {
+        let val = e.target.value;
+        setEmail( val );
+
+    };
+
+    const changePassword = ( e ) => {
+        let val = e.target.value;
+        setPassword( val );
+
+    };
+
+    const validateEmail = ( val ) => {
+        let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ( filter.test( val )){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    const validatePassword = ( val ) => {
+        
+        let errors = [];
+        if ( val.length < 8 ) {
+            errors.push("Ваш пароль должен содержать не менее 8 символов"); 
+        };
+        if (val.search(/[a-z]/i) < 0) {
+            errors.push("Ваш пароль должен содержать хотя бы одну букву.");
+        };
+        if (val.search(/[0-9]/) < 0) {
+            errors.push("Ваш пароль должен содержать хотя бы одну цифру."); 
+        };
+        if (errors.length > 0) {
+            alert(errors.join("\n"));
+            return false;
+        };
+        return true;
+    }
+
+
+    const send = () => {
+        let email_trim = email.trim();
+        let password_trim = password.trim();
+
+        // if( validateEmail( email_trim ) ){
+        //     if( validatePassword( password_trim ) ){
+                console.dir( email_trim );
+                console.dir( password_trim );
+
+                send_request_to_server({
+                    route: 'login-by-post',
+                    data: {
+                        // email: email_trim,
+                        // password: password_trim,
+                        email: email.trim(),
+                        password: password.trim(),
+                    },
+                    callback: ( resp ) => {
+                        console.dir( 'resp' );
+                        console.dir( resp );
+                    },
+                });
+
+        //     }else{
+        //         console.dir( `не валиден ${password_trim}` );
+        //     };
+        // }else{
+        //     console.dir( `не валиден ${email_trim}` );
+        // }
+
+    };
+
     return (
-        <PageContainer
-            className = 'loginPage'
-            page =      { ROUTE.PAGE.LOGIN }
-        >
+        <PageContainer className = 'loginPage'>
+
+            <div className = 'loginPage_wrap'>
+
+                <div className = 'loginPage_item'>
+                    <h2>Логин:</h2>
+                    <input 
+                        type =      'email'
+                        className = ''
+                        value =     { email }
+                        onChange =  { changeEmail }
+                    />
+                </div>
+
+                <div className = 'loginPage_item'>
+                    <h2>Пароль:</h2>
+                    <input 
+                        type =      'password'
+                        className = ''
+                        value =     { password }
+                        autoComplete = 'current-password'
+                        minLength = { 6 }
+                        maxLength = { 20 }
+
+                        onChange =  { changePassword }
+                    />
+
+                    <div className = 'btn'>
+                        <span
+                            onClick = { send }
+                        >Отправить</span>
+                    </div>
+                </div>
+
+
+            </div>
 
 
         </PageContainer>
